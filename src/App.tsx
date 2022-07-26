57dfe3a08e46e774f5b32c01ac4b5b9a45d4c5b6
Eric Zhuang <ezhuang13@gmail.com>
2022-07-26 14:50:14 -0700

    example

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import React from "react";
+import Calendar from "./Calendar.tsx";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Calendar.tsx
@@ -0,0 +1,99 @@
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+
+// https://momentjs.com/docs/
+import moment, { MomentInput } from "moment";
+import React, { FC, useState } from "react";
+import { DayPickerRangeController, isInclusivelyBeforeDay } from "react-dates";
+
+export interface DateRange {
+  startDate: moment.Moment | null | undefined;
+  endDate: moment.Moment | null | undefined;
+}
+
+const START_DATE = "startDate";
+const END_DATE = "endDate";
+type FocusedInput = "startDate" | "endDate";
+
+type ComparatorOp = ">" | "<" | "==" | ">=" | "<=";
+
+const Calendar: FC = ({}) => {
+  // ------- Used for implementation. Can probably ignore these -------
+  const [focusedInput, setFocusedInput] = useState<FocusedInput>(START_DATE);
+  const handleDatesChange = (newDateRange: DateRange) => {
+    const { startDate: newStartDate, endDate: newEndDate } = newDateRange;
+    setStartDate(newStartDate?.toDate());
+    setEndDate(newEndDate?.toDate());
+  };
+  const handleFocusChange = (newFocusedInput: FocusedInput) => {
+    if (newFocusedInput === END_DATE) setEndDate(null);
+    setFocusedInput(newFocusedInput || START_DATE);
+  };
+  const isOutsideRange = (day: moment.Moment) =>
+    isInclusivelyBeforeDay(day, moment().subtract(1, "days"));
+
+  // ------- Some helpful functions and variables -------
+  const campsiteReservedDays = ["2022-07-30"]; // Modify as needed for testing
+  const [startDate, setStartDate] = useState<Date | undefined | null>();
+  const [endDate, setEndDate] = useState<Date | undefined | null>();
+  const compareDates = (
+    date1: MomentInput,
+    date2: MomentInput,
+    comparator: ComparatorOp
+  ) => {
+    switch (comparator) {
+      case ">":
+        return moment(date1).isAfter(date2, "day");
+      case "<":
+        return moment(date1).isBefore(date2, "day");
+      case "==":
+        return moment(date1).isSame(date2, "day");
+      case ">=":
+        return moment(date1).isSameOrAfter(date2, "day");
+      case "<=":
+        return moment(date1).isSameOrBefore(date2, "day");
+    }
+  };
+
+  // ------- TODO: implement this function -------
+  const isDayBlocked = (calendarDay: moment.Moment) => {
+    return false;
+    // ------- Example solution -------
+    // if (endDate && compareDates(calendarDay, endDate, "==")) return false;
+
+    // // isDayBlocked for site calendar
+    // return campsiteReservedDays.some((reservedDay) => {
+    //   // If a day is reserved, that means you cannot check in on that day or check out any days
+    //   // after the reserved day
+    //   const isCheckOutDay =
+    //     startDate &&
+    //     !endDate &&
+    //     compareDates(moment(reservedDay), startDate, ">");
+    //   if (isCheckOutDay) {
+    //     const dayBefore = calendarDay.clone().subtract(1, "day");
+    //     return compareDates(dayBefore, reservedDay, ">=");
+    //   }
+    //   return compareDates(calendarDay, reservedDay, "==");
+    // });
+  };
+
+  return (
+    <>
+      <DayPickerRangeController
+        verticalHeight={200}
+        hideKeyboardShortcutsPanel={true}
+        numberOfMonths={2}
+        startDate={startDate ? moment(startDate) : null}
+        endDate={endDate ? moment(endDate) : null}
+        orientation={"horizontal"}
+        isOutsideRange={isOutsideRange}
+        onDatesChange={handleDatesChange}
+        focusedInput={focusedInput}
+        onFocusChange={handleFocusChange}
+        isDayBlocked={isDayBlocked}
+      />
+    </>
+  );
+};
+
+export default Calendar;
